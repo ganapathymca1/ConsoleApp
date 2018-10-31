bc3e0ba97a3dc9f318707251e89e20105e69a1ef
ganapathy <ganapathy_mca@hotmail.com>
2018-10-30 22:13:59 -0400

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:57616/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("values");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
